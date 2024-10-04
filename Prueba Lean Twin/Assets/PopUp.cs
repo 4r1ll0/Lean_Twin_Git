260f64fafc52dc21929723cc6e0df87d74023405
4r1ll0 <ivan.diaz0306@gmail.com>
2024-10-04 09:51:00 +0100

    04/10/2024

--- Prueba Lean Twin/Assets/PopUp.cs
+++ Prueba Lean Twin/Assets/PopUp.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PopUp : MonoBehaviour
@@ -13,6 +14,7 @@ public class PopUp : MonoBehaviour
     [SerializeField]
     LeanTweenType animCurve;
 
+
     void Start()
     {
         background.SetActive(false);
@@ -21,13 +23,44 @@ public class PopUp : MonoBehaviour
 
     void Update()
     {
-        
-        if(Input.GetKeyUp(KeyCode.Escape))
+
+        if (Input.GetKeyUp(KeyCode.Escape))
         {
-            popup.SetActive(true);
-            background.SetActive(true);
-            LeanTween.moveLocalY(popup, -900f, 0f);
-            LeanTween.moveLocalY(popup, 0f, timeAnim).setEase(animCurve);
+            if (popup.activeSelf)
+            {
+                HidePopUp();
+            }
+            else
+            {
+                ShowPopUp();
+            }
         }
     }
+
+    //Funcion
+    public void ShowPopUp()
+    {
+        popup.SetActive(true);
+        background.SetActive(true);
+        LeanTween.moveLocalY(popup, -900f, 0f);
+        LeanTween.moveLocalY(popup, 0f, timeAnim).setEase(animCurve);
+    }
+
+    public void HidePopUp()
+    {
+        LeanTween.moveLocalY(popup, -900f, timeAnim).setEase(animCurve).setOnComplete(() => { 
+        
+            popup.SetActive(false);
+        });
+        LeanTween.alphaCanvas(background.GetComponent<CanvasGroup>(), 0f, timeAnim).setOnComplete(() =>
+        {
+            background.SetActive(false);
+            //poner el alpha canva a 1 , hacerlo en 0 segs
+            LeanTween.alphaCanvas(background.GetComponent<CanvasGroup>(), 1f, 0f);
+        });
+    }
+
 }
+    
+
+
